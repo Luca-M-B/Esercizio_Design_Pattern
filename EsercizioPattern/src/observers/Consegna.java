e616550a6326268fe53f9d058e2f94d6c1691127
MazzottaLuca <lucamazzotta99@gmail.com>
2026-04-08 16:37:09 +0200

    .

---
+++ EsercizioPattern/src/observers/Consegna.java
@@ -0,0 +1,7 @@
+public class Consegna implements Observer {
+    public void aggiorna(String stato) {
+        if (stato.equals("PRONTO")) {
+            System.out.println("pizza pronta alla consegna");
+        }
+    }
+}
