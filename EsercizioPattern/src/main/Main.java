ae0d986289d34c55b03bd9334c701e56581a2740
LucaMB <lmbaratti@gmail.com>
2026-04-08 17:10:45 +0200

    .

--- EsercizioPattern/src/main/Main.java
+++ EsercizioPattern/src/main/Main.java
@@ -1,11 +1,13 @@
 package main;
 
-
-
 import java.util.Scanner;
 
 import classi.Ordine;
+import pizze_base.BaseBianca;
+import pizze_base.BaseRossa;
+import pizze_base.Margherita;
 import pizze_base.Pizza;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -17,47 +19,52 @@ public class Main {
 
         switch (scelta) {
             case 1:
-            case 2:
-            case 3:
-            default:
-                System.out.println("scegli una opzione valida");
+                pizza = new BaseRossa();
                 break;
+            case 2:
+                pizza = new BaseBianca();
+                break;
+            case 3:
+                pizza = new Margherita();
+                break;
+            default:
+                System.out.println("Scelta non valida.");
         }
 
-        /* // Singleton
-        GestoreOrdini gestore = GestoreOrdini.getInstance();
-        gestore.aggiungiOrdine(ordine); */
-        
-        /*// Cambia stati
-        ordine.setStato("IN PREPARAZIONE");
-        ordine.setStato("IN COTTURA");
-        ordine.setStato("PRONTO");
- */
+        /*
+         * // Singleton
+         * GestoreOrdini gestore = GestoreOrdini.getInstance();
+         * gestore.aggiungiOrdine(ordine);
+         */
 
+        /*
+         * // Cambia stati
+         * ordine.setStato("IN PREPARAZIONE");
+         * ordine.setStato("IN COTTURA");
+         * ordine.setStato("PRONTO");
+         */
 
-
-
-        /*  
-        Main:
-
-        Loop con menu:
-         - Ordina pizza
-         - Traccia ordine
-         - Modifica ordine
-         - Elimina ordine
-         - Esci
-        
-         - Sotto menu per admin:
-            - Menu pizze:
-                - Aggiungi pizza
-                - Visualizza pizze
-                - Cerca pizza
-                - Modifica pizza
-                - Rimuovi pizza
-                - Torna al menu admin
-            - Menu ordini
-            - Torna al main menu
-        */
+        /*
+         * Main:
+         * 
+         * Loop con menu:
+         * - Ordina pizza
+         * - Traccia ordine
+         * - Modifica ordine
+         * - Elimina ordine
+         * - Esci
+         * 
+         * - Sotto menu per admin:
+         * - Menu pizze:
+         * - Aggiungi pizza
+         * - Visualizza pizze
+         * - Cerca pizza
+         * - Modifica pizza
+         * - Rimuovi pizza
+         * - Torna al menu admin
+         * - Menu ordini
+         * - Torna al main menu
+         */
         Ordine ordine = new Ordine(pizza);
         // aspetto per chiamare i metodi aggiungiObserver
         /*
